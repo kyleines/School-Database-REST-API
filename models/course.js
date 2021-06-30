d8ac38b7ca346e5bcd1275d8d7ae57b5adbafa54
kyleines <kyleines@gmail.com>
2021-06-30 01:51:02 -0500

    update course model with attributes and validation

--- models/course.js
+++ models/course.js
@@ -7,5 +7,40 @@ const Sequelize = require("sequelize");
 // Creates Course model
 module.exports = (sequelize) => {
     class Course extends Sequelize.Model {}
-    Course.init();
+    Course.init({
+        title: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: "A title is required",
+                },
+                notEmpty: {
+                    msg: "Please provide a title",
+                },
+            },
+        },
+        description: {
+            type: Sequelize.TEXT,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: "A description is required",
+                },
+                notEmpty: {
+                    msg: "Please provide a description",
+                },
+            },
+        },
+        estimatedTime: {
+            type: Sequelize.STRING, 
+        },
+        materialsNeeded: {
+            type: Sequelize.STRING, 
+        },
+    },
+    {
+        sequelize,
+    });
+    return Course;
 }
