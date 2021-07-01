205e8c0cf65e52075e55bda7699de0d83520b522
kyleines <kyleines@gmail.com>
2021-06-30 20:35:10 -0500

    update routes with /api/courses/:id get route

--- routes/index.js
+++ routes/index.js
@@ -44,6 +44,21 @@ router.get("/courses", asyncHandler(async (req, res) => {
 
 // A /api/courses/:id GET route that will return the corresponding
 //  course including the User associated with that course and a 200 HTTP status code.
+router.get("/courses/:id", asyncHandler(async (req, res) => {
+    const courses = await Course.findAll({
+        include: [
+            {
+                model: User,
+                as: "user",
+            }
+        ],
+        where: {
+            id: req.params.id,
+        },
+    });
+
+    res.status(200).json(courses.map(course => course.get({plain: true})));
+}));
 
 // A /api/courses POST route that will create a new course, set the
 //  Location header to the URI for the newly created course, and return a 201 HTTP status code and no content.
